0edce5fe8a7c9c72f79687540e7e6b7d7ec712fe
Dmitriy Nevzorov <jimmy.lugat@gmail.com>
2020-09-22 15:49:09 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# MONEI NODE SDK
+
+[MONEI](https://monei.net/) is a Digital Payment Gateway.
+
+Accept online payments without astronomical transaction fees. Take advantage of the best payment gateway rates to help you grow your e-commerce business.
+
+## Install
+
+```
+npm i @monei-js/node-sdk --save
+```
+
+or
+
+```
+yarn add @monei-js/node-sdk
+```
+
+For the full documentation check our [Documentation portal](https://docs.monei.net/)
---
+++ monei.ts
@@ -0,0 +1,51 @@
+import axios, {AxiosResponse} from 'axios';
+import {Configuration, PaymentsApi} from './src';
+import {BASE_PATH} from './src/base';
+
+export * from './src';
+
+const instance = axios.create();
+
+type ServerErrorResponse = {
+  status: string;
+  statusCode: number;
+  requestId: string;
+  message: string;
+  requestTime: string;
+};
+
+class ServerError extends Error {
+  status: string;
+  statusCode: number;
+  requestId: string;
+  requestTime: Date;
+
+  constructor(res: ServerErrorResponse) {
+    super(res.message);
+    this.status = res.status;
+    this.statusCode = res.statusCode;
+    this.requestId = res.requestId;
+    this.requestTime = new Date(res.requestTime);
+  }
+}
+
+const responseHandler = (res: AxiosResponse) => {
+  return res.data;
+};
+
+const errorHandler = (error: any) => {
+  if (error?.response?.data) {
+    throw new ServerError(error.response.data);
+  }
+  throw new Error('Something when wrong');
+};
+
+instance.interceptors.response.use(responseHandler, errorHandler);
+
+export class Monei {
+  payments: PaymentsApi;
+
+  constructor(apiKey: Configuration['apiKey']) {
+    this.payments = new PaymentsApi({apiKey}, BASE_PATH, instance);
+  }
+}
