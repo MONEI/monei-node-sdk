8a358d7adfe99b394b16cb14575c787b6a1153db
Dmitriy <dn@monei.com>
2025-03-07 11:57:29 +0100

    chore: Modify commitlint configuration to disable subject case rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-line-length': [2, 'always', 100],
-    'footer-max-line-length': [2, 'always', 100],
-    'header-max-length': [2, 'always', 100]
+    'subject-case': [0, 'never']
   }
 };
